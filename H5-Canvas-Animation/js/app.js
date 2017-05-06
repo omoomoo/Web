2f93686e5853f18a570920cb8a9f950dffcdc8d9
cokepluscarbon <93948886@qq.com>
2017-05-06 16:01:32 +0800

    H5-Animcation 02-*

--- H5-Canvas-Animation/js/app.js
+++ H5-Canvas-Animation/js/app.js
@@ -1,22 +1,25 @@
 var jSeal = {
-	acquireMouse: function(element){
-		var mouse = {x: 0, y: 0};
-		$(element).mousemove(function(event){
+	acquireMouse: function(element) {
+		var mouse = {
+			x: 0,
+			y: 0
+		};
+		$(element).mousemove(function(event) {
 			mouse.x = event.offsetX;
 			mouse.y = event.offsetY;
 		});
-		
+
 		return mouse;
 	}
 };
 
-function Arrow(){
+function Arrow() {
 	this.x = 0;
 	this.y = 0;
 	this.color = '#03A9F4';
 	this.rotation = 0; // Math.PI = 180deg
 };
-Arrow.prototype.draw = function (context) {
+Arrow.prototype.draw = function(context) {
 	context.save();
 	context.translate(this.x, this.y);
 	context.rotate(this.rotation);
@@ -36,3 +39,26 @@ Arrow.prototype.draw = function (context) {
 	context.stroke();
 	context.restore();
 };
+
+function Ball(radius) {
+	this.x = 0;
+	this.y = 0;
+	this.radius = radius || 10;
+	this.color = '#03A9F4';
+	this.rotation = 0;
+}
+
+Ball.prototype.draw = function(ctx) {
+	ctx.save();
+	ctx.fillStyle = this.color;
+	ctx.translate(this.x, this.y);
+	ctx.rotate(this.rotation);
+		
+	ctx.beginPath();
+	ctx.arc(0, 0, 10, 0, Math.PI * 2);
+	ctx.closePath();
+	
+	ctx.stroke();
+	ctx.fill();
+	ctx.restore();
+}
